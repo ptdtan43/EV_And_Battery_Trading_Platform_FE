c969793dd12d898e846bc61ebef794a24054af7c
Tan <ptdtan43@gmail.com>
2025-11-12 16:47:37 +0700

    none

--- SimpleRatingApi/obj/Debug/net9.0/SimpleRatingApi.AssemblyInfo.cs
+++ SimpleRatingApi/obj/Debug/net9.0/SimpleRatingApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SimpleRatingApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e6bacb19a8739a166d5c33bfbafff390c143880e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e081c4c4c44140d0c0413fccb2a46d30803c007a")]
 [assembly: System.Reflection.AssemblyProductAttribute("SimpleRatingApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SimpleRatingApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
